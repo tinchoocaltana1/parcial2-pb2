2555fe92d8c6af57470f25be3f521cb0d92f7dbf
Martin Caltana <tinchocaltana@gmail.com>
2025-11-18 20:28:14 -0300

    Commit inicial1

---
+++ CriaturasElementales/src/ar/edu/unlam/pb2/CriaturasElementales/Criatura.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.pb2.CriaturasElementales;
+
+public class Criatura {
+
+}
---
+++ CriaturasElementales/test/ar/edu/unlam/pb2/CriaturasElementales/CriaturaTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.CriaturasElementales;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CriaturaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
